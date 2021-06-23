2133f76d366f478a37db9b5b13aed3127f6b18ec
Simon Adamov <simon.adamov@mailbox.org>
2021-06-23 20:12:27 +0200

    Peer-Review #1

--- data-raw/preproc.R
+++ data-raw/preproc.R
@@ -53,7 +53,10 @@ sixhour <- pollen_raw$hourly %>%
     )
   ) %>%
   group_by(date, hour, trap) %>%
-  summarise(conc = mean(conc)) %>%
+  summarise(conc = if_else(sum(is.na(conc)) <= 1,
+    mean(conc, na.rm = TRUE),
+    NA_real_
+  )) %>%
   ungroup() %>%
   mutate(datetime = as_datetime(
     paste0(
@@ -69,7 +72,11 @@ sixhour <- pollen_raw$hourly %>%
 # Daily aggregation of hourly values
 daily <- pollen_raw$hourly %>%
   group_by(trap, date) %>%
-  summarise(conc = mean(conc)) %>%
+  mutate(missing_values = sum(is.na(conc))) %>%
+  summarise(conc = if_else(sum(is.na(conc)) <= 5,
+    mean(conc, na.rm = TRUE),
+    NA_real_
+  )) %>%
   ungroup() %>%
   mutate(
     datetime = as_datetime(date),
@@ -91,55 +98,72 @@ pollen_full <- list(
       as_datetime("2019-04-19 00:00:00"),
       as_datetime("2019-05-31 23:00:00")
     )) %>%
+    arrange(desc(trap), date) %>%
     pivot_wider(names_from = trap, values_from = conc) %>%
     # The standard compare against - mean of 2 Hirst traps
     # Internal evaluation found that on average the sucking rate is higher
     # than described by the manufacturer
     mutate(hirst = (hirst1 + hirst2) / 2 / 1.35) %>%
     select(-hirst1, -hirst2) %>%
-    pivot_longer(KHA:hirst, names_to = "trap", values_to = "conc"))
+    pivot_longer(wibs:hirst, names_to = "trap", values_to = "conc") %>%
+    arrange(trap))
 
-# To be very conservative, days with calibration events
-# have been excluded for all traps. 2019 was the year of
-# initial deployment, hence there are quite a few days to be excluded.
-pollen <- pollen_full %>%
-  map(~ .x %>%
-    filter(
-      !(date %in% c(
-        # Here Poleno 1  was undergoing calibration
-        date("2019-04-20"),
-        date("2019-04-21"),
-        date("2019-04-22"),
-        date("2019-04-25"),
-        date("2019-04-29"),
-        date("2019-05-09"),
-        date("2019-05-10"),
-        date("2019-05-14"),
-        date("2019-05-15"),
-        date("2019-05-27"),
-        date("2019-05-31"),
-        # Here Poleno 1  was undergoing calibration
-        date("2019-05-13"),
-        date("2019-05-14"),
-        date("2019-05-15"),
-        date("2019-05-16"),
-        date("2019-05-26"),
-        date("2019-05-27"),
-        # Here Hirst2 has missing data
-        date("2019-04-23"),
-        date("2019-05-31"),
-        # Here Rapide had a software issue (values provided in chunks)
-        date("2019-04-22"),
-        date("2019-04-23"),
-        date("2019-04-24"),
-        date("2019-04-25"),
-        date("2019-04-26"),
-        date("2019-05-01"),
-        date("2019-05-07"),
-        date("2019-05-17"),
-        date("2019-05-24")
-      ))
-    ))
+# # To be very conservative, days with calibration events
+# # have been excluded for all traps. 2019 was the year of
+# # initial deployment, hence there are quite a few days to be excluded.
+# pollen <- pollen_full %>%
+#   map(~ .x %>%
+#     filter(
+#       !(date %in% c(
+#         # Here Poleno 1  was undergoing calibration
+#         date("2019-04-20"),
+#         date("2019-04-21"),
+#         date("2019-04-22"),
+#         date("2019-04-25"),
+#         date("2019-04-29"),
+#         date("2019-05-09"),
+#         date("2019-05-10"),
+#         date("2019-05-14"),
+#         date("2019-05-15"),
+#         date("2019-05-27"),
+#         date("2019-05-31"),
+#         # Here Poleno 1  was undergoing calibration
+#         date("2019-05-13"),
+#         date("2019-05-14"),
+#         date("2019-05-15"),
+#         date("2019-05-16"),
+#         date("2019-05-26"),
+#         date("2019-05-27"),
+#         # Here Hirst2 has missing data
+#         date("2019-04-23"),
+#         date("2019-05-31"),
+#         # Here Rapide had a software issue (values provided in chunks)
+#         date("2019-04-22"),
+#         date("2019-04-23"),
+#         date("2019-04-24"),
+#         date("2019-04-25"),
+#         date("2019-04-26"),
+#         date("2019-05-01"),
+#         date("2019-05-07"),
+#         date("2019-05-17"),
+#         date("2019-05-24")
+#       ))
+#     ))
+
+# After peer-review it was suggested to not exclude the full days, but rather calculate the mean concentrations as long as enough values are present.
+# For the sixhour averages we at least 4 values must be !na and for the daily averages at least 16 must be !na.
+# This threshold of 66% availability of data is somewhat arbitrary.
+
+
+pollen <- map(pollen_full, ~.x %>%
+  group_by(datetime) %>%
+  mutate(set_na = if_else(any(is.na(conc)), TRUE, FALSE)) %>%
+  ungroup() %>%
+  mutate(conc = if_else(set_na, NA_real_, conc)) %>%
+  filter(!is.na(conc)) %>%
+  arrange(trap))
+
+
 
 usethis::use_data(pollen, overwrite = TRUE)
 usethis::use_data(pollen_full, overwrite = TRUE)
